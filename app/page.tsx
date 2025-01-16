a72db46c14642354449154827686ea025f09d369
Yash Avsarmal <134092522+yashavsarmal30@users.noreply.github.com>
2025-01-16 22:15:23 +0530

    r

--- app/page.tsx
+++ app/page.tsx
@@ -82,10 +82,10 @@ export default function Home() {
       </div>
 
       <footer className="py-4 flex justify-center pb-8" >
-        <p className="footer-text text-sm flex items-center gap-1" > 
+        <a className="footer-text text-sm flex items-center gap-1" > 
            href={"https://yashavsarmal.netlify.app/"} target="_blank"
           WeatherPro - An Weather App
-        </p>
+        </a>
 
       </footer>
 
